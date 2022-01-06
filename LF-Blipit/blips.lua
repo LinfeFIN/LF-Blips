2e45d3ec683e51769af2c27cb7271f5c52a3a95b
LinfeFIN <64391067+LinfeFIN@users.noreply.github.com>
2022-01-06 02:50:56 +0200

    Update blips.lua

--- LF-Blipit/blips.lua
+++ LF-Blipit/blips.lua
@@ -1,12 +1,7 @@
 ESX = nil
 local paalla = false
 local AllBlips = {}
-Citizen.CreateThread(function()
-	while ESX == nil do
-		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		Citizen.Wait(0)
-	end
-end)
+
 
 RegisterCommand("ShopsON", function(source, args, rawCommand)
 	paalla = true
